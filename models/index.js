f2e17fe1ee7b0fa5e82e83ae486b39eff9214fe0
unknown <53538316+grlucy@users.noreply.github.com>
2020-05-08 16:47:38 -0400

    created non-API route

--- models/index.js
+++ models/index.js
@@ -1,22 +1,23 @@
-const mongoose, {Schema} = require("mongoose");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
- category: {
-  type: String,
-  required: true,
-  enum: ["Produce", "Meats", "Dry Goods", "Refrigerated", "Other"]
- },
- name: {
-  type: String,
-  trim: true,
-  required: true
- },
- status: {
-  type: Boolean,
-  required: true,
-  default: false
- }
-})
+  category: {
+    type: String,
+    required: true,
+    enum: ["Produce", "Meats", "Dry Goods", "Refrigerated", "Other"],
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  status: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
 
 const Item = mongoose.model("Item", itemSchema);
 
