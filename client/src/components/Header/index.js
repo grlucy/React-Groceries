61009daad493497cccc0ed18d17388d866fc6e3b
unknown <53538316+grlucy@users.noreply.github.com>
2020-05-09 14:34:39 -0400

    styled Header component

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,11 +2,19 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
+    <header className="bg-light">
       <div className="container">
         <div className="row">
           <div className="col text-center">
-            <p>Grocery List</p>
+            <p
+              className="mt-3"
+              style={{
+                fontFamily: "Covered By Your Grace, cursive",
+                fontSize: "45px",
+              }}
+            >
+              Grocery List
+            </p>
           </div>
         </div>
       </div>
